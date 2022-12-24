0e26d445bdb3ec42f5b7f634d244d88645e93b6c
SONU SAH <>
2022-12-24 13:02:44 +0530

    adding memories

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js';
+
+
+const app = express();
+
+app.use('/post', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+
+const CONNECTION_URL = 'mongodb+srv://rohit_gupta:Ubuntu567@memoriesapi.kcgybqf.mongodb.net/memoriesAPI? retryWrites = true & w=majority';
+const PORT = process.env.PORT || 4001;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+
+.then(() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
+
+    .catch((error) => console.log(error.message))
+
+mongoose.set('useFindAndModify', true);
